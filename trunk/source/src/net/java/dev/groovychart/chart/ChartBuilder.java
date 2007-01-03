26ecd6982892854e08a70686fa57e65e066247d0
jimclarke <jimclarke@8d77b76f-85b1-ec8f-b2f1-8127c7b3c1ff>
2007-01-03 00:38:50 +0000

    Initial Check in, moved dataset java files to their own package

--- trunk/source/src/net/java/dev/groovychart/chart/ChartBuilder.java
+++ trunk/source/src/net/java/dev/groovychart/chart/ChartBuilder.java
@@ -1,4 +1,19 @@
 /*
+ *
+ * Copyright 2006 Sun Microsystems, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  * ChartBuilder.java
  *
  * Created on November 11, 2006, 8:43 PM
@@ -19,6 +34,9 @@ import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import net.java.dev.groovychart.dataset.category.DefaultCategorySetBuilder;
+import net.java.dev.groovychart.dataset.series.xy.DefaultXYDatasetBuilder;
+import net.java.dev.groovychart.dataset.category.JDBCCategoryDatasetBuilder;
 import org.jfree.chart.JFreeChart;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.general.Dataset;
---
+++ trunk/source/src/net/java/dev/groovychart/dataset/series/xy/DefaultOHLCDatasetBuilder.java
@@ -0,0 +1,69 @@
+/*
+ *
+ * Copyright 2006 Sun Microsystems, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * DefaultOHLCDatasetBuilder.java
+ *
+ * Created on December 7, 2006, 3:11 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package net.java.dev.groovychart.dataset.series.xy;
+
+import java.util.Map;
+import net.java.dev.groovychart.dataset.BaseDatasetBuilder;
+import org.jfree.data.general.Dataset;
+import org.jfree.data.xy.DefaultOHLCDataset;
+
+/**
+ *
+ * @author jclarke
+ */
+public class DefaultOHLCDatasetBuilder extends BaseDatasetBuilder {
+    
+    /** Creates a new instance of DefaultOHLCDatasetBuilder */
+    public DefaultOHLCDatasetBuilder() {
+    }
+    
+    public Dataset getDataset() {
+        return getXyDataset();
+    }    
+
+    /**
+     * Holds value of property xyDataset.
+     */
+    private DefaultOHLCDataset xyDataset;
+
+    /**
+     * Getter for property xyDataset.
+     * @return Value of property xyDataset.
+     */
+    public DefaultOHLCDataset getXyDataset() {
+        return this.xyDataset;
+    }
+
+    /**
+     * Setter for property xyDataset.
+     * @param xyDataset New value of property xyDataset.
+     */
+    public void setXyDataset(DefaultOHLCDataset xyDataset) {
+        this.xyDataset = xyDataset;
+    }
+
+    public void processNode(Object name, Map map, Object value) throws Exception {
+    }
+}
